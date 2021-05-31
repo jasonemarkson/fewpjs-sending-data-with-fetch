bcb2c6362b5b11fae6dc60559aa1bafefe88c31a
jasonemarkson <jasonmarkson76@gmail.com>
2021-05-31 12:40:38 -0400

    commit tree 7e2bc1067f81a1345f754a1ef78dedcd04f8d095

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Add your code here
+// // Add your code here
+let destinationURL = 'http://localhost:3000/users'
+
+
+// function submitData(name, email) {
+//     return fetch(destinationURL, configObject)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         document.body.innerHTML = object["id"];
+//     })
+//     .catch(function(error) {
+//         document.body.innerHTML = error.message;
+//     });
+// }
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
